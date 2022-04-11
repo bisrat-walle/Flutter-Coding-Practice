5d224c7b9080bb5380cb764e339503d3584ea330
bisrat-walle <bisratwalle3@gmail.com>
2022-04-11 21:38:14 +0300

    adding grid view screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'profile1.dart';
+import 'grid_view_practice.dart';
 
 void main() {
   runApp(
 		MaterialApp(
 		  title: "Profile App",
 		  debugShowCheckedModeBanner: false,
-		  home: ProfileOne()
+		  home: GridViewDemo()
 		  ),
 		);
 }
